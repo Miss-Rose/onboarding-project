8804638a36c3eb218cfcdd59b8aa330921e85f03
tstepanova <tstepanova@griddynamics.com>
2021-11-26 11:24:19 +0200

    fix indents

--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
-import { App } from "./app";
-import { ZoomerView } from "./views/ZoomerView";
+import { App } from './app';
+import { ZoomerView } from './views/ZoomerView';
+import { CountView } from './views/CountView';
+import Backbone from 'backbone';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const app = new App();
-    app.addRegions({
-        zoomer: '#zoomImages',
-    });
-    app.on('start', function() {
-        Backbone.history.start();
-    });
-    app.start();
-    app.zoomer.show(new ZoomerView());
-})
+  const app = new App();
+  app.addRegions({
+    zoomer: '#zoomImages',
+    card: '#cartView',
+  });
+  app.on('start', function() {
+    Backbone.history.start();
+  });
+  app.start();
+  app.zoomer.show(new ZoomerView());
+  app.card.show(new CountView());
+});
