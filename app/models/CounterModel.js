fd255d4cdc67df924ff6b3cc9f337e619c40bb61
tstepanova <tstepanova@griddynamics.com>
2021-11-30 14:33:08 +0200

    fix counter model

--- app/models/CounterModel.js
+++ app/models/CounterModel.js
@@ -2,11 +2,13 @@ import Backbone from 'backbone';
 import Radio from "backbone.radio/build/backbone.radio";
 
 const myChannel = Radio.channel('card');
+const store = window.localStorage.getItem('count') ?
+    JSON.parse(window.localStorage.getItem('count')).length : 0;
 
 const CounterModel = Backbone.Model.extend({
   defaults: function() {
     return {
-      count: JSON.parse(window.localStorage.getItem('count')).length
+      count: store
     };
   },
 });
@@ -14,7 +16,6 @@ const CounterModel = Backbone.Model.extend({
 const counter = new CounterModel();
 myChannel.on('save', (num) => {
   counter.set('count', num);
-  console.log(num)
 });
 
 export default counter;
